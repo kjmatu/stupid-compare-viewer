68ba54afd77aea1ed70de610de141eb5b93a7a59
kjmatu <kenji.matsunaga@me.com>
2024-07-30 23:58:04 +0900

    change svelte5 style

--- src/lib/Greet.svelte
+++ src/lib/Greet.svelte
@@ -1,15 +1,15 @@
 <script>
     import { invoke } from '@tauri-apps/api/tauri'
 
-    let name = ''
-    let greetMsg = ''
+    let name = $state("");
+    let greetMsg = $state("");
     async function greet() {
         greetMsg = await invoke('greet', { name })
     }
 </script>
 
 <div>
-    <input id="greet-input" placeholder="Enter a name..." bind:value="{name}" />
-    <button on:click="{greet}">Greet</button>
+    <input id="greet-input" placeholder="Enter a name..." bind:value={name} />
+    <button onclick={greet}>Greet</button>
     <p>{greetMsg}</p>
 </div>
