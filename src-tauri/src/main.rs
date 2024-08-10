97a4ac17700d5e89a70c6e9f7bd8947e758c401f
kjmatu <kenji.matsunaga@me.com>
2024-08-10 16:48:35 +0900

    change return type

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -2,16 +2,44 @@
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
 use tauri::api::dialog::blocking::FileDialogBuilder;
+use serde::Serialize;
+use std::path::PathBuf;
+use std::fs::File;
+use std::io::Read;
+use base64::encode;
 
+
+#[derive(Serialize)]
+#[serde(rename_all = "camelCase")]
+struct ImageData {
+  file_path: PathBuf,
+  base64_data: String,
+  width: u32,
+  height: u32,
+}
 
 #[tauri::command]
-async fn open_image()  -> Result<String, String> {
+async fn open_image()  -> Result<ImageData, String> {
   let dialog_result = FileDialogBuilder::new().pick_file();
   match dialog_result {
     Some(path) => {
-      return Ok(path.display().to_string());
+      let mut file = File::open(path.clone()).map_err(|e| e.to_string())?;
+      let mut buffer = Vec::new();
+      file.read_to_end(&mut buffer).map_err(|e| e.to_string())?;
+      let image_data = ImageData {
+        file_path: path,
+        base64_data: encode(&buffer),
+        width: 0,
+        height: 0,
+      };
+      Ok(image_data)
     },
-    None => Ok("No file selected".to_string()),
+    None => Ok(ImageData{
+      file_path: "".into(),
+      base64_data: "".to_string(),
+      width: 0,
+      height: 0,
+    }),
   }
 }
 
--- src/lib/Image.svelte
+++ src/lib/Image.svelte
@@ -2,12 +2,14 @@
     import { invoke } from '@tauri-apps/api/tauri'
 
     let selectedFileName = $state("");
+    let imageData = $state("");
     async function openImage() {
-        selectedFileName = await invoke('open_image')
+        imageData = await invoke('open_image')
     }
 </script>
 
 <div>
     <button onclick={openImage}>OPEN</button>
     <p>{selectedFileName}</p>
+    <img src={`data:image/jpeg;base64,${imageData.base64Data}`} alt="src">
 </div>
