7990796d3f8f560b6cd093bd0d8802faa18a1acb
kjmatu <kenji.matsunaga@me.com>
2024-08-10 22:49:08 +0900

    use LoadedImageData class as type

--- src/lib/Image.svelte
+++ src/lib/Image.svelte
@@ -1,15 +1,22 @@
-<script>
+<script lang="ts">
     import { invoke } from '@tauri-apps/api/tauri'
+	import type { LoadedImageData } from './../../src-tauri/bindings/LoadedImageData';
 
-    let selectedFileName = $state("");
-    let imageData = $state("");
+    let loadedImageData: LoadedImageData = $state(
+        {
+            "filePath": "",
+            "base64Data": "",
+            "height": 0,
+            "width": 0
+        }
+    )
     async function openImage() {
-        imageData = await invoke('open_image')
+        loadedImageData = await invoke('open_image')
     }
 </script>
 
 <div>
     <button onclick={openImage}>OPEN</button>
-    <p>{selectedFileName}</p>
-    <img src={`data:image/jpeg;base64,${imageData.base64Data}`} alt="src">
-</div>
+    <p>{loadedImageData.filePath}</p>
+    <img src={`data:image/jpeg;base64,${loadedImageData.base64Data}`} alt="src">
+</div> 
