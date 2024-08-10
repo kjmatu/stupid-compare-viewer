a7c23dcb995dc48539cb3fcd89b3f99301926e85
kjmatu <kenji.matsunaga@me.com>
2024-08-10 15:37:28 +0900

    refactor ui codes

---
+++ src/lib/Image.svelte
@@ -0,0 +1,13 @@
+<script>
+    import { invoke } from '@tauri-apps/api/tauri'
+
+    let selectedFileName = $state("");
+    async function openImage() {
+        selectedFileName = await invoke('open_image')
+    }
+</script>
+
+<div>
+    <button onclick={openImage}>OPEN</button>
+    <p>{selectedFileName}</p>
+</div>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,6 +1,6 @@
 <script>
-    import Greet from '../lib/Greet.svelte'
+    import Image from '../lib/Image.svelte'
 </script>
 
 <h1>Welcome to Stupid Compare Viewer</h1>
-<Greet />
+<Image />
