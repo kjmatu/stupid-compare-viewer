46672c56e01e2c63119f3c96dc57b6d6ae444daa
kjmatu <kenji.matsunaga@me.com>
2024-08-18 22:50:56 +0900

    change display source from base64 data to file path

--- src/lib/Image.svelte
+++ src/lib/Image.svelte
@@ -1,5 +1,5 @@
 <script lang="ts">
-    import { invoke } from '@tauri-apps/api/tauri'
+    import { invoke, convertFileSrc } from '@tauri-apps/api/tauri';
 	import type { LoadedImageData } from './../../src-tauri/bindings/LoadedImageData';
 
     let loadedImageData: LoadedImageData = $state(
@@ -18,5 +18,5 @@
 <div>
     <button onclick={openImage}>OPEN</button>
     <p>width={loadedImageData.width}, height={loadedImageData.height} {loadedImageData.filePath}</p>
-    <img src={`data:image/jpeg;base64,${loadedImageData.base64Data}`} alt="src">
+    <img src={`${convertFileSrc(loadedImageData.filePath)}`} alt="selected img">
 </div> 
