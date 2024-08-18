af52a8ce86d99317f4d1ffe526773a70759c33e2
kjmatu <kenji.matsunaga@me.com>
2024-08-18 23:36:53 +0900

    move file load feature from Image component

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,5 +1,22 @@
-<script>
-    import Image from '../lib/Image.svelte'
+<script lang="ts">
+    import Image from '../lib/Image.svelte';
+    import { invoke } from '@tauri-apps/api/tauri';
+
+	import type { LoadedImageData } from './../../src-tauri/bindings/LoadedImageData';
+
+
+    let loadedImageData: LoadedImageData = $state(
+        {
+            "filePath": "",
+        }
+    )
+    async function openImage() {
+        loadedImageData = await invoke('open_image')
+    }
+
+
 </script>
 
-<Image />
+<button onclick={openImage}>OPEN</button>
+
+<Image leftImagePath={loadedImageData.filePath} rightImagePath={loadedImageData.filePath} />
