aafe6afe3058f218d957fcdbcfda139366f9ca92
kjmatu <kenji.matsunaga@me.com>
2024-09-21 00:55:26 +0900

    get image paths from dialog selected directory path

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -1,8 +1,11 @@
 // Prevents additional console window on Windows in release, DO NOT REMOVE!!
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
-use tauri::api::dialog::blocking::FileDialogBuilder;
+use std::fs;
 use std::path::PathBuf;
+
+use tauri::api::dialog::blocking::FileDialogBuilder;
+
 use serde::Serialize;
 use ts_rs::TS;
 
@@ -14,13 +17,35 @@ struct LoadedImageData {
   file_path: PathBuf,
 }
 
+
+fn get_all_image_files_in_dir(path: PathBuf, target_extensions: Vec<&str>) -> Vec<PathBuf> {
+  let mut file_paths = Vec::new();
+  for entry in fs::read_dir(path).unwrap() {
+    let entry = entry.unwrap();
+    let path = entry.path();
+    if path.is_file() {
+      match path.extension() {
+        Some(extension) => {
+          if target_extensions.contains(&extension.to_ascii_lowercase().to_str().unwrap()) {
+            file_paths.push(path);
+          }
+        },
+        None => continue,
+      }
+    }
+  }
+  file_paths
+}
+
 #[tauri::command]
 async fn open_image()  -> Result<LoadedImageData, String> {
-  let dialog_result = FileDialogBuilder::new().pick_file();
+  let dialog_result = FileDialogBuilder::new().pick_folder();
   match dialog_result {
     Some(path) => {
+      let file_paths = get_all_image_files_in_dir(path, vec!["jpg", "jpeg", "png", "heic"]);
+      println!("{:?}", file_paths);
       let image_data = LoadedImageData {
-        file_path: path,
+        file_path: file_paths[0].clone(),
       };
       Ok(image_data)
     },
